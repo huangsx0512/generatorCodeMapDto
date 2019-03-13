7c7cf043b4815dacfa7a514484d18aac52403a30
qux <quxing10190933@163.com>
2019-03-13 09:29:54 +0800

    删除部分文件

--- temp_src/com/tsingning/worksite/dao/mapper/RecFolderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.tsingning.worksite.dao.mapper;
-
-import tk.mybatis.mapper.common.Mapper;
-import com.tsingning.worksite.dao.model.RecFolder;
-public interface RecFolderMapper extends Mapper<RecFolder>{
-	
-}
--- temp_src/com/tsingning/worksite/service/IRecFolderService.java
+++
@@ -1,5 +0,0 @@
-package com.tsingning.worksite.service;
-
-public interface IRecFolderService{ 
-	
-}
